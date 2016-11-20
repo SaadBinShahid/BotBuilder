99ad84dcb00c756a5d916fe44c7403c90904defa
Saad Bin Shahid <saadbinshahid.fb@gmail.com>
2016-11-20 13:30:00 +0500

    Integrated PizzaBot in SimpleEchoBot.

--- CSharp/Samples/SimpleEchoBot/Controllers/MessagesController.cs
+++ CSharp/Samples/SimpleEchoBot/Controllers/MessagesController.cs
@@ -11,6 +11,8 @@ using Microsoft.Bot.Builder.Dialogs.Internals;
 using Autofac;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.Bot.Sample.PizzaBot;
+using Microsoft.Bot.Builder.FormFlow;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -33,6 +35,68 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+
+        private static IForm<PizzaOrder> BuildForm()
+        {
+            var builder = new FormBuilder<PizzaOrder>();
+
+            ActiveDelegate<PizzaOrder> isBYO = (pizza) => pizza.Kind == PizzaOptions.BYOPizza;
+            ActiveDelegate<PizzaOrder> isSignature = (pizza) => pizza.Kind == PizzaOptions.SignaturePizza;
+            ActiveDelegate<PizzaOrder> isGourmet = (pizza) => pizza.Kind == PizzaOptions.GourmetDelitePizza;
+            ActiveDelegate<PizzaOrder> isStuffed = (pizza) => pizza.Kind == PizzaOptions.StuffedPizza;
+
+            return builder
+                // .Field(nameof(PizzaOrder.Choice))
+                .Field(nameof(PizzaOrder.Size))
+                .Field(nameof(PizzaOrder.Kind))
+                .Field("BYO.Crust", isBYO)
+                .Field("BYO.Sauce", isBYO)
+                .Field("BYO.Toppings", isBYO)
+                .Field(nameof(PizzaOrder.GourmetDelite), isGourmet)
+                .Field(nameof(PizzaOrder.Signature), isSignature)
+                .Field(nameof(PizzaOrder.Stuffed), isStuffed)
+                .AddRemainingFields()
+                .Confirm("Would you like a {Size}, {BYO.Crust} crust, {BYO.Sauce}, {BYO.Toppings} pizza?", isBYO)
+                .Confirm("Would you like a {Size}, {&Signature} {Signature} pizza?", isSignature, dependencies: new string[] { "Size", "Kind", "Signature" })
+                .Confirm("Would you like a {Size}, {&GourmetDelite} {GourmetDelite} pizza?", isGourmet)
+                .Confirm("Would you like a {Size}, {&Stuffed} {Stuffed} pizza?", isStuffed)
+                .Build()
+                ;
+        }
+
+        internal static IDialog<PizzaOrder> MakeRoot()
+        {
+            return Chain.From(() => new PizzaOrderDialog(BuildForm))
+                                .Do(async (context, order) =>
+                                {
+                                    try
+                                    {
+                                        var completed = await order;
+                                        // Actually process the sandwich order...
+                                        await context.PostAsync("Processed your order!");
+                                    }
+                                    catch (FormCanceledException<PizzaOrder> e)
+                                    {
+                                        string reply;
+                                        if (e.InnerException == null)
+                                        {
+                                            reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                                        }
+                                        else
+                                        {
+                                            reply = "Sorry, I've had a short circuit.  Please try again.";
+                                        }
+                                        await context.PostAsync(reply);
+                                    }
+                                });
+        }
+
+        public static bool isOrderingPizza = false;
+        private static void ProcessOrder()
+        {
+            isOrderingPizza = false;
+        }
+
         /// <summary>
         /// POST: api/Messages
         /// receive a message from a user and send replies
@@ -44,7 +108,16 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             // check if activity is of type message
             if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
-                HandleUserMessage(activity);
+                if (activity.Text == "No, Order a pizza" || isOrderingPizza)
+                {
+                    isOrderingPizza = true;
+                    //await Conversation.SendAsync(activity, MakeRoot);
+                    await Conversation.SendAsync(activity, MakeRoot);
+                }
+                else
+                {
+                    HandleUserMessage(activity);
+                }
             }
             else
             {
